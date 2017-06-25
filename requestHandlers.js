f744b90877107166a03ce1f38e2f5f4b3a62720b
Charlie Zhu <goodgoodwish@gmail.com>
2017-06-24 23:18:56 -0700

    Event-driven asynchronous callbacks.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,25 +1,34 @@
-function start() {
-	console.log('Request handler "start" was called.')
+var exec = require('child_process').exec;
 
-	function sleep (milliSeconds) {
-		var startTime = new Date().getTime();
-		while (new Date().getTime() < startTime + milliSeconds) {
-			
-		}
-		console.log('sleep ' + milliSeconds);
-	}
-	sleep(11000);
-	return 'Hello Juliet';
+function start(response) {
+  console.log('Request handler "start" was called.')
+  var content = 'empty';
+
+  // ls -lah = dir on DOS,
+  exec('dir', {timeout: 10000, maxBuffer: 20000 * 1024}, function(error, stdout, stderr) {
+    content = stdout;
+    console.log(content);
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write(stdout);
+    response.end();
+    console.log('ls -lah');
+  })
+
+  return content;
 }
 
-function upload() {
-	console.log('Request handler "upload" was called.')
-	return 'Hello upload';
+function upload(response) {
+  console.log('Request handler "upload" was called.')
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write('Hello upload');
+  response.end();
 }
 
-function submitForm() {
-	console.log('Request handler "submitForm" was called.')
-	return 'Hello submitForm';
+function submitForm(response) {
+  console.log('Request handler "submitForm" was called.')
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write('Hello submitForm');
+  response.end();
 }
 
 exports.start = start;
