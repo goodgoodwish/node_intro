c5068f08a37a5c3d41ced6f7444e51bedbe2c0a9
Charlie Zhu <goodgoodwish@gmail.com>
2017-06-26 22:36:52 -0700

    Process POST request.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,41 @@
+var querystring = require('querystring');
 var exec = require('child_process').exec;
 
 function start(response) {
   console.log('Request handler "start" was called.')
+  var body='<html>' +
+  '<head>' +
+  '<meta charset=UTF-8/>' +
+  '</head>' +
+  '<body>' +
+  '<form action="/upload" method="post">' +
+  '<textarea name="text" rows="20" cols="70"></textarea><br>' +
+  '<input type="submit" value="Submit text" />' +
+  '</form>' +
+  '</body>' +
+  '</html>'
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(body);
+    response.end();
+}
+
+function upload(response, postData) {
+  console.log('Request handler "upload" was called.')
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write('You have sent the text: ' + querystring.parse(postData).text);
+  response.end();
+}
+
+function submitForm(response) {
+  console.log('Request handler "submitForm" was called.')
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write('Hello submitForm');
+  response.end();
+}
+
+function osDir(response) {
+  console.log('Request handler "osDir" was called.')
   var content = 'empty';
 
   // ls -lah = dir on DOS,
@@ -14,22 +48,8 @@ function start(response) {
     console.log('ls -lah');
   })
 
-  return content;
 }
 
-function upload(response) {
-  console.log('Request handler "upload" was called.')
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello upload');
-  response.end();
-}
-
-function submitForm(response) {
-  console.log('Request handler "submitForm" was called.')
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello submitForm');
-  response.end();
-}
 
 exports.start = start;
 exports.upload = upload;
